34857057ef76b530e9f79fb09a17a1a7465a7e44
Reddysumanth2000 <56265436+Reddysumanth2000@users.noreply.github.com>
2024-06-23 22:26:55 -0700

    Adding up and down buttons

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let flashcards = [];
 let currentCard = 0;
 let flipped = false;
+let upl = [
+    0, 1, 3, 4, 5, 6, 7,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70
+];
+
 
 function loadFlashcards() {
     fetch('flashcards.json')
@@ -59,6 +65,47 @@ document.getElementById("Back").addEventListener("click", () => {
     }
     updateCard();
 });
+document.getElementById("Up").addEventListener("click", () => {
+    if (currentCard >= 0 && !upl.includes(currentCard)) {
+        if(currentCard< 8){
+            currentCard= currentCard-2;
+        }
+        else if(currentCard<=20){
+            currentCard=currentCard-8;
+        }
+        else if(currentCard<=58){
+            currentCard=currentCard-18;
+        }
+        else {
+            currentCard=currentCard-32;
+        }}
+
+     else {
+        alert(' No element above in the table');
+    }
+    updateCard();
+});
+document.getElementById("Down").addEventListener("click", () => {
+    if (currentCard <= 85) {
+        if(currentCard== 0 ){
+            currentCard+=3;
+        }
+        else if(currentCard<= 12){
+            currentCard= currentCard+8;
+        }
+        else if(currentCard<=39){
+            currentCard+=18;
+        }
+        else {
+            currentCard+=32;
+        }}
+
+     else {
+        alert(' No element below in the table');
+    }
+    updateCard();
+});
+
 document.getElementById("random").addEventListener("click", () => {
     currentCard = Math.floor(Math.random() * 120) + 1;
     updateCard();
